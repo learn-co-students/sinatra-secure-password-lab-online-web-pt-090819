73b8a667e5254b4c01c7a76fee3852c826881a8c
Sarah Condon <s.j.condon01@gmail.com>
2020-01-02 14:38:38 -0600

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+   has_secure_password
+   def up
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+    end
+  end
+  
+  def down
+    drop_table :users
+  end
 end
